6db356814113dd1250f6ff3d0e4b59793dd9b134
kassrojas <kassandrarojass@gmail.com>
2022-12-03 16:02:50 -0500

    added start button and some styling, got timer up and running

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var startEl = document.getElementById('startButton');
+var timerEl = document.getElementById('timer');
 var nextEl = document.querySelector('#next');
 var cursor = 0;
 var questions = [
@@ -8,8 +10,11 @@ var questions = [
 
 
 var displayQuestion = function () {
+    countdownTimer(); //starts timer at the same time as question
     nextEl.textContent = questions[cursor];
 };
+
+startEl.addEventListener('click', displayQuestion);
 
 var advance = function (){
     if (cursor < questions.length - 1) {
@@ -19,6 +24,51 @@ var advance = function (){
 
 };
 
+
 nextEl.addEventListener('click', advance);
 
+
+// countdownTimer() works to keep track of the timer. setInterval(function () {what to do}, 1000) acts on secondsLeft to go down by 1 second every 1000m and to clearInterval() || stop the timerInterval || when secondsLeft === 0.   
+
+function countdownTimer(){
+    var secondsLeft = 40;
+    displayTime(secondsLeft);
+    
+    // console.log(secondsLeft);
+    
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        displayTime(secondsLeft);
+        
+        
+        if (secondsLeft === 0){
+            clearInterval(timerInterval);
+            gameOverMessage();
+            timerEl.textContent = '';
+        }
+        
+    }, 1000);
+}
+
+function displayTime(secondsLeft){
+    var timeLabel = 'seconds';
+
+    if (secondsLeft === 1) {
+        timeLabel = 'second';
+    }
+
+    timerEl.textContent = 'Time left: ' + secondsLeft + ' ' + timeLabel;
+}
+
+function gameOverMessage() {
+    // create a function that displays a game over message
+}
+
+
+
+
+
+
+
+countdownTimer();
 displayQuestion();
