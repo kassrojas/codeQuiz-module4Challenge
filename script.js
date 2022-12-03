54638d96b54311a9676f0d058c8466c61887eb51
kassrojas <kassandrarojass@gmail.com>
2022-12-03 16:23:47 -0500

    deleted question object in script-- going html route instead

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ var startEl = document.getElementById('startButton');
 var timerEl = document.getElementById('timer');
 var nextEl = document.querySelector('#next');
 var cursor = 0;
-var questions = [
-    "Did you have a break?",
-    "Do you like icecream?", 
-    "What's your favorite pizza topping?", 
-    "What's your favorite weekday?"]
 
 
 var displayQuestion = function () {
