10779b4a026830180e31896279b676e3cda5bdfd
kassrojas <kassandrarojass@gmail.com>
2022-12-04 16:09:41 -0500

    adding some logic to displayQuestion()

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var startEl = document.querySelector('#startButton');
 var timerEl = document.querySelector('#timer');
-var questionEl = document.querySelector('.question');
+var questionsEl = document.querySelector('.questions');
 var cursor = 0;
+var score = 0;
 
 var questionsArray = [
     {
@@ -12,7 +13,7 @@ var questionsArray = [
             "b. after </body> element",
             "c. within <head> element",
             "d. after </html> element",
-        ]
+        ],
     },
     {
         questionText: "Event delegation targets: ",
@@ -22,7 +23,7 @@ var questionsArray = [
             "b. a single element on the page",
             "c. a common parent element",
             "d. all click events",
-        ]
+        ],
     },
     {
         questionText: "What is the proper syntax for a function in Javascript?",
@@ -33,7 +34,7 @@ var questionsArray = [
             "c. var = someName function () {};",
             "d. all of the above",
             "e. only a and b",
-        ]
+        ],
     },
     {
         questionText: "What information is stored within the () of function someName () {}; ?",
@@ -43,7 +44,7 @@ var questionsArray = [
             "b. if else statements",
             "c. logic of the function",
             "d. parameters to be passed into the function",
-        ]
+        ],
     },
     {
         questionText: "What is the proper syntax for creating an array?",
@@ -53,30 +54,37 @@ var questionsArray = [
             "b. var items = {item1, item2}; ",
             "c. var items = ('item1', 'item2')",
             "d. var items = [item1; item2]; ",
-        ]
+        ],
     },
 
 ];
 
-var correctAnswers = ['q1a', 'q2c', 'q3d', 'q4b', 'q5a', 'q6c'];
 
 var displayQuestion = function () {
     countdownTimer(); 
     
-    questionEl.querySelector('h2').textContent = questionsObject[cursor].text;
+    questionsEl.querySelector('h3').textContent = questionsArray[cursor].questionText;
+    questionsEl.querySelector('#possible').innerHTML = null;
    
-}
+    for (var buttonLabel of questionsArray[cursor].possible){
+        var buttonEl = document.createElement('button');
+        buttonEl.textContent = buttonLabel;
+        buttonEl.dataset.choice = buttonLabel[0];
+        questionsEl.querySelector('#possible').appendChild(buttonEl);
+    }
+};
 
 // startEl.addEventListener('click', displayQuestion);
 
 var advance = function (event){
     var element = event.target;
 
-    if (element.matches('.question button')){
-        element.dataset.choice === correctAnswers[cursor];
+    if (element.matches('.questions button')){
+        element.dataset.choice === questionsArray[cursor].answer;
         
     if (cursor < questionsEl.length - 1) {
         cursor++;
+        questionsEl.dataset.index = cursor;
     }
         displayQuestion();
     }
@@ -88,7 +96,6 @@ document.addEventListener('click', advance);
 
 
 // countdownTimer() works to keep track of the timer. setInterval(function () {what to do}, 1000) acts on secondsLeft to go down by 1 second every 1000m and to clearInterval() || stop the timerInterval || when secondsLeft === 0.   
-
 function countdownTimer(){
     var secondsLeft = 60;
     displayTime(secondsLeft);
@@ -109,6 +116,7 @@ function countdownTimer(){
     }, 1000);
 }
 
+//displayTime() maintains proper labels on countdownTimer
 function displayTime(secondsLeft){
     var timeLabel = 'seconds';
 
@@ -125,4 +133,4 @@ function gameOverMessage() {
 
 
 
-// displayQuestion();
+displayQuestion();
