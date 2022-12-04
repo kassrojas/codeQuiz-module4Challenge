cb5d5243a68e344dd18b4e9c82499ffb106dcfae
kassrojas <kassandrarojass@gmail.com>
2022-12-04 15:39:29 -0500

    added questions array... going javascript route instead of using HTML bc it was too confusing

--- script.js
+++ script.js
@@ -1,32 +1,96 @@
-var startEl = document.getElementById('startButton');
-var timerEl = document.getElementById('timer');
-var nextEl = document.querySelector('#next');
+var startEl = document.querySelector('#startButton');
+var timerEl = document.querySelector('#timer');
+var questionEl = document.querySelector('.question');
 var cursor = 0;
 
+var questionsArray = [
+    {
+        questionText: "Javascript files should be inserted:",
+        answer: "a",
+        possible: [
+            "a. before </body> element",
+            "b. after </body> element",
+            "c. within <head> element",
+            "d. after </html> element",
+        ]
+    },
+    {
+        questionText: "Event delegation targets: ",
+        answer: "c",
+        possible: [
+            "a. only the children elements",
+            "b. a single element on the page",
+            "c. a common parent element",
+            "d. all click events",
+        ]
+    },
+    {
+        questionText: "What is the proper syntax for a function in Javascript?",
+        answer: "e",
+        possible: [
+            "a. var someName = function () {}; ", //function expression
+            "b. function someName () {}; ", //function declaration
+            "c. var = someName function () {};",
+            "d. all of the above",
+            "e. only a and b",
+        ]
+    },
+    {
+        questionText: "What information is stored within the () of function someName () {}; ?",
+        answer: "d",
+        possible: [
+            "a. console.log();",
+            "b. if else statements",
+            "c. logic of the function",
+            "d. parameters to be passed into the function",
+        ]
+    },
+    {
+        questionText: "What is the proper syntax for creating an array?",
+        answer: "a",
+        possible: [
+            "a. var items = ['item1', 'item2']; ",
+            "b. var items = {item1, item2}; ",
+            "c. var items = ('item1', 'item2')",
+            "d. var items = [item1; item2]; ",
+        ]
+    },
+
+];
+
+var correctAnswers = ['q1a', 'q2c', 'q3d', 'q4b', 'q5a', 'q6c'];
 
 var displayQuestion = function () {
-    countdownTimer(); //starts timer at the same time as question
-    nextEl.textContent = questions[cursor];
-};
+    countdownTimer(); 
+    
+    questionEl.querySelector('h2').textContent = questionsObject[cursor].text;
+   
+}
 
-startEl.addEventListener('click', displayQuestion);
+// startEl.addEventListener('click', displayQuestion);
 
-var advance = function (){
-    if (cursor < questions.length - 1) {
+var advance = function (event){
+    var element = event.target;
+
+    if (element.matches('.question button')){
+        element.dataset.choice === correctAnswers[cursor];
+        
+    if (cursor < questionsEl.length - 1) {
         cursor++;
+    }
         displayQuestion();
     }
 
 };
 
-
-nextEl.addEventListener('click', advance);
+document.addEventListener('click', advance);
+// advances us on any click
 
 
 // countdownTimer() works to keep track of the timer. setInterval(function () {what to do}, 1000) acts on secondsLeft to go down by 1 second every 1000m and to clearInterval() || stop the timerInterval || when secondsLeft === 0.   
 
 function countdownTimer(){
-    var secondsLeft = 40;
+    var secondsLeft = 60;
     displayTime(secondsLeft);
     
     // console.log(secondsLeft);
@@ -61,9 +125,4 @@ function gameOverMessage() {
 
 
 
-
-
-
-
-countdownTimer();
-displayQuestion();
+// displayQuestion();
